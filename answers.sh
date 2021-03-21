8a8794b27b7f65d24b7a268b1445084866c679a0
omar abdallah <omarlb512@gmail.com>
2021-03-21 22:27:24 +0200

    Creating Table

--- answers.sh
+++ answers.sh
@@ -1,3 +1,4 @@
+
 1-"SELECT name FROM students";
 2-"SELECT * FROM students WHERE Age>30;"
 3-"SELECT * FROM students WHERE age =30 AND gender = "F";"
@@ -6,4 +7,28 @@
 6-"UPDATE students SET Points= Points+30 WHERE name="Basma";"
 7-"UPDATE students SET Points= Points-30 WHERE name="Alex";"
 
+#Creat a table graduates 
 
+CREATE TABLE "graduates" (
+	"ID"	INTEGER NOT NULL,
+	"Name"	TEXT NOT NULL UNIQUE,
+	"Age"	INTEGER,
+	"Gender"	TEXT,
+	"Points"	INTEGER,
+	"Graduation" TEXT,
+	PRIMARY KEY("ID" AUTOINCREMENT)
+);
+
+#1.Copy Layal's data from students to graduates
+INSERT INTO graduates (ID,Name,Age,Gender,Points)
+SELECT *,
+FROM students,
+WHERE Name = "Layal";
+
+#2.Add the graduation date previously mentioned to Layal's record in graduates
+UPDATE graduates
+SET Graduation = "08/09/2018";
+
+#3.Remove Layal's record from students
+DELETE FROM students 
+WHERE Name="Layal";
