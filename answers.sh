39ca27cc2b4ee91d1233517ae7a518e4b736dd73
omar abdallah <omarlb512@gmail.com>
2021-03-21 21:38:05 +0200

    Basic Queries

---
+++ answers.sh
@@ -0,0 +1,9 @@
+1-"SELECT name FROM students";
+2-"SELECT * FROM students WHERE Age>30;"
+3-"SELECT * FROM students WHERE age =30 AND gender = "F";"
+4-"SELECT Points FROM students WHERE students.name = "Alex";"
+5-"INSERT INTO students (ID,Name,Age,Gender,Points) VALUES (8,"omar abdallah",25,"Male",60);"
+6-"UPDATE students SET Points= Points+30 WHERE name="Basma";"
+7-"UPDATE students SET Points= Points-30 WHERE name="Alex";"
+
+
